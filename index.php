fe8d398162f7418431e656a2233149da66afbee5
LaPoloche <buchon.paul@gmail.com>
2023-01-05 12:05:48 +0100

    début tinder

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
     <h1 id="startTitle">Bienvenue dans l’aventure MMI: TindRPG</h1>
     <img id="imgStart" src="./assets/Logo.png" alt="TindRPG">
     <p id="textStart">Cette application a pour but de vous guider tout au long de cette porte ouverte pour la formation MMI</p>
-    <button id="boutonDebut">C'est parti</button>
+    <button id="boutonDebut" onclick="location.href='tinder.php';" >C'est parti</button>
 </body>
 </html>
---
+++ tinder.php
@@ -0,0 +1,11 @@
+<?php include_once('header.php')?>
+<body style="
+justify-content: initial;
+">
+    <div class="toplogo">
+        <img class="applogo" src="./assets/Logo.png" alt="TindRPG">
+        <h2 class="logotitle">TindRPG</h2>
+    </div>
+    
+</body>
+</html>
