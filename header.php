81d1069a52d04d7127260758fcf67295ebdb993f
Quentin Ratinaud <quentin.ratinaud@gmail.com>
2023-01-09 17:06:21 +0100

    header

--- header.php
+++ header.php
@@ -1,3 +1,6 @@
+<?php
+    $file_name = basename($_SERVER["SCRIPT_FILENAME"], '.php');
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -5,8 +8,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer">
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="./css/paul.css">
+    <link rel="stylesheet" href=".css/global.css">
+    <link rel="stylesheet" href=".css/components.css">
+    <?php
+        $own_style_path = "style/" . $file_name . ".css";
+        if(file_exists($own_style_path)) echo "<link rel='stylesheet' href='$own_style_path'>";
+    ?>
     <title>TindRPG</title>
 </head>
 <?php
