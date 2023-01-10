f2fba086c09cc61faecbe9185986ed8de6092cf4
LaPoloche <buchon.paul@gmail.com>
2023-01-10 09:54:15 +0100

    Tinder fini

--- carte.php
+++ carte.php
@@ -1,3 +1,4 @@
 <?php
+require_once('header.php');
 var_dump($_SESSION['mmiquest']['prof_id']);
 ?>
--- tinder_result.php
+++ tinder_result.php
@@ -7,11 +7,6 @@ justify-content: initial;
 ">
 <p class="td_text" style="display: none;">Je vois que tu aime <?php echo(get_prof_by_id($_GET['pid'])['spe'])?></p>
 <p class="pid" style="display: none;"><?php echo($_GET['pid'])?></p>
-
-<div class="td_toplogo">
-        <img class="td_applogo" src="./assets/Logo.png" alt="TindRPG">
-        <h2 class="td_logotitle">TindRPG</h2>
-    </div>
     <?php component("components/logo.php") ?>
     <div class="td_dialog">
         <img class="td_avatar" src="<?php echo(get_prof_by_id($_GET['pid'])['img'])?>" alt="<?php echo(get_prof_by_id($_GET['pid'])['nom'])?>">
@@ -44,12 +39,15 @@ justify-content: initial;
         }
         else if(dg_count==3){
             console.log('slt')
-            location.href = "save_prof.php?pid="+document.querySelector('#pid').innerHTML;
+            location.href = "save_prof.php?pid="+ document.querySelector('.pid').innerHTML;
         }
         dg_count++
     }
 </script>
-
+    <!-- <div class="td_toplogo">
+        <img class="td_applogo" src="./assets/Logo.png" alt="TindRPG">
+        <h2 class="td_logotitle">TindRPG</h2>
+    </div> -->
 
         <!-- <img class="td_avatar" src="<?php echo(get_prof_by_id($_GET['pid'])['img'])?>" alt="<?php echo(get_prof_by_id($_GET['pid'])['nom'])?>">
         <div class="td_avatar_dialog">
