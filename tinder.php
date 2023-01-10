c532cb24d6565fc9db495889beec31efa47b4552
LaPoloche <buchon.paul@gmail.com>
2023-01-10 08:38:27 +0100

    dilaogie tinder

--- tinder.php
+++ tinder.php
@@ -79,23 +79,3 @@ justify-content: initial;
         })
     })
 </script>
-
-
-
-
-
-
-
-        <!-- <div class="td_card">
-            <div class="td_questions_container">
-                <div class="question">Serais-tu attiré par le marketing?</div>
-            </div>
-            <div class="td_action_buttons">
-                <div class="td_button td_back_button">
-                    <img class="td_button_img td_back_img" src="./assets/icons/redcross.png" alt="Cancel">
-                </div>
-                <div class="td_button td_like_button">
-                    <img class="td_button_img td_like_img" src="./assets/icons/heart.png" alt="Like">
-                </div>
-            </div>
-        </div> -->
--- tinder_result.php
+++ tinder_result.php
@@ -10,7 +10,18 @@ justify-content: initial;
         <img class="td_applogo" src="./assets/Logo.png" alt="TindRPG">
         <h2 class="td_logotitle">TindRPG</h2>
     </div>
-    
-    <img src="<?php echo(get_prof_by_id($_GET['pid'])['img'])?>" alt="<?php echo(get_prof_by_id($_GET['pid'])['nom'])?>">
+    <div class="td_dialog">
+        <img class="td_avatar" src="<?php echo(get_prof_by_id($_GET['pid'])['img'])?>" alt="<?php echo(get_prof_by_id($_GET['pid'])['nom'])?>">
+        <div class="td_avatar_dialog">
+            <p class="td_text_dialog">Salut, moi c'est <?php echo(get_prof_by_id($_GET['pid'])['nom'])?></p>
+            <!-- <img src="assets/questions/bulle.png" alt=""> -->
+        </div>
+    </div>
+    <div class="td_avatar_act_button">
+        <button>Suivant</button>
+    </div>
 </body>
 </html>
+<script>
+    
+</script>
