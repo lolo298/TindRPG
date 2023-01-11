9b94871056d72a0e2bc4aab36af6074e56d1b1bf
lolo298 <lorenzoaoulini@gmail.com>
2023-01-11 12:14:24 +0100

    ajout de la spe dans la BDD

--- save_prof.php
+++ save_prof.php
@@ -2,12 +2,14 @@
 require_once('header.php');
 $_SESSION['mmiquest']['prof_id'] = $_GET['pid'];
 $nom = explode(" ", get_prof_by_id($_SESSION['mmiquest']['prof_id'])['nom']);
+$spe = get_prof_by_id($_SESSION['mmiquest']['prof_id'])['spe'];
 ?>
 
 <script>
     
  db.collection("Users").doc(sessionStorage.getItem("id")).update({
-    "avatar": '<?= $nom[0] ?>'
+    "avatar": '<?= $nom[0] ?>',
+    "matiere": '<?= $spe ?>',
  }).then(()=>{
     document.location.href = "./carte.php";
  })
