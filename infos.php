f08fa7878739bc46aa26a9faba4d8e4a61fffa00
LaPoloche <buchon.paul@gmail.com>
2023-01-11 22:10:50 +0100

    ptit changement

--- infos.php
+++ infos.php
@@ -4,6 +4,7 @@ include_once('header.php');
 ?>
 <body>
     <?php component("components/logo.php") ?>
+    <button value="prof" class="quit btn blanc"><span>◄</span><p>Retour à la carte</p></button>
     <div class="in_buttons_container">
         <button value="prof" class="in_but btn"><p>Professeur</p><span>►</span></button>
         <button value="salles" class="in_but btn"><p>Salles</p><span>►</span></button>
@@ -162,4 +163,7 @@ include_once('header.php');
 
         }
     }))
+    document.querySelector('.quit').addEventListener('click',function(){
+        location.href = 'carte.php'
+    })
 </script>
