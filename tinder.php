4809a290d69d601e7c1da8a9c1b9658f0d0175b6
lolo298 <lorenzoaoulini@gmail.com>
2023-01-17 09:02:12 +0100

    unknown avatar si sélectionner trop vite

--- tinder.php
+++ tinder.php
@@ -33,22 +33,22 @@ justify-content: initial;
 </html>
 <script>
     const answ_to_prof = {
-        0: "0", 
+        0: "1", 
         1: "2",
-        2: "0",
+        2: "1",
         3: "2",
         4: "4",
         5: "4",
         6: "6",
-        7: "0",
+        7: "1",
         8: "7",
         9: "3",
         10: "5",
         11: "6",
         12: "3",
         13: "2",
-        14: "0",
-        15: "1",
+        14: "1",
+        15: "0",
     };
     let lsitcards = document.querySelectorAll('.td_card')
     let likebut = document.querySelectorAll('.td_like_button');
@@ -67,7 +67,9 @@ justify-content: initial;
 
     function add_result(data){
         result += data
-        if(result.length == 4){
+        console.log(result)
+        if(result.length >= 4){
+            result = result.substring(0, 4);
             setTimeout(function(){
                 window.location.href = "tinder_result.php?pid="+answ_to_prof[parseInt(result, 2)];
             }, 1000)
